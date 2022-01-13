2e478c3fd5bc3108c3fb95d442165400088318be
jsblair9 <james@lionsmouth.digital>
2022-01-13 17:33:51 -0600

    🔬 try using the path instead of Buffer for img

--- src/lib/twitter/notifyTwitter.ts
+++ src/lib/twitter/notifyTwitter.ts
@@ -4,15 +4,15 @@ import axios from 'axios';
 
 export default async function notifyTwitter(twitterClient: TwitterAPI, nftSale: NFTSale) {
     const hasImage = Boolean(nftSale.nftData?.image);
-    const data     = hasImage ? await getImageDataFromUrl(nftSale.nftData?.image as string) : null;
+    const imgUrl = nftSale.nftData?.image;
+    // const data     = hasImage ? await getImageDataFromUrl(nftSale.nftData?.image as string) : null;
     const nftName  = nftSale.nftData?.name;
     const text     = `${nftName} was just purchased for ${nftSale.getPriceInSOL()} S◎L on ${nftSale.marketplace.name}! Now go and enroll @degenDAOO to meet your classmates.`
 
     const mediaArr: string[] = [];
     
     if (hasImage) {
-        console.log(data as Buffer);
-        const media = await twitterClient.v1.uploadMedia(data as Buffer, { type: 'png' });
+        const media = await twitterClient.v1.uploadMedia(`${imgUrl}`, { type: 'png' });
         mediaArr.push(media);
     }
 
@@ -21,7 +21,7 @@ export default async function notifyTwitter(twitterClient: TwitterAPI, nftSale: 
       });
 }
 
-async function getImageDataFromUrl(url: string) {
-    const img = await axios.get(url, {responseType: 'arraybuffer'});
-    return img.data as Buffer
-}
+// async function getImageDataFromUrl(url: string) {
+//     const img = await axios.get(url, {responseType: 'arraybuffer'});
+//     return img.data as Buffer
+// }
