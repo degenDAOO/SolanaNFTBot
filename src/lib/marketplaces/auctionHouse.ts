59ddc477d7881292852d4ca97f1ab08220ef9b06
jsblair9 <james@lionsmouth.digital>
2022-04-18 10:43:42 -0500

    🐛 tweet length

--- src/lib/marketplaces/auctionHouse.ts
+++ src/lib/marketplaces/auctionHouse.ts
@@ -2,10 +2,10 @@ import { Marketplace, NFTSale } from "./types";
 import { parseNFTSaleOnTx } from "./helper";
 
 const auctionHouse: Marketplace = {
-  name: "AuctionHouse (Rarible or OpenSea)",
+  name: "Auction House (Rarible or OpenSea)",
   programId: ["hausS13jsjafwWwGqZTUQRmWyvyxn9EQpqMwV1PBBmk"],
   twitterHandle: "@DegenTrackerBot",
-  iconUrl: "https://docs.metaplex.com/img/meta-black.svg",
+  iconUrl: "https://assets.website-files.com/6182ee30b608385a15466a3f/6184383c556990103357ea77_favicon.png",
   itemURL: (token: String) => `https://marketplace.degenape.academy/token/SOLANA:${token}`,
   parseNFTSale(web3Conn, txResp): Promise<NFTSale | null> {
     return parseNFTSaleOnTx(web3Conn, txResp, this);
